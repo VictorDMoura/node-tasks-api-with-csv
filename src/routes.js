1452943e27d70aca06544863b83333de3ae1a6dc
VictorDMoura <mouramvictor@gmail.com>
2024-11-17 22:36:25 -0300

    add delee method

--- src/routes.js
+++ src/routes.js
@@ -58,4 +58,14 @@ export const routes = [
       return res.writeHead(204).end();
     },
   },
+
+  {
+    method: "DELETE",
+    path: buildRoutePath("/tasks/:id"),
+    handler: async (req, res) => {
+      const { id } = req.params;
+      database.delete(TABELE_DATABASE, id);
+      return res.writeHead(204).end();
+    },
+  },
 ];
