7716c1ef044d293d39e92f0978ab441574d780b9
VictorDMoura <mouramvictor@gmail.com>
2024-11-19 15:09:18 -0300

    add read csv file

--- src/stream/read-file-csv.js
+++ src/stream/read-file-csv.js
@@ -1,8 +1,20 @@
 import fs from "node:fs";
 import { parse } from "csv-parse";
+import { randomUUID } from "node:crypto";
 
 const csvFilePath = new URL("../../tasks.csv", import.meta.url);
-
+const TABELE_DATABASE = "tasks";
 export async function saveCsvFileToDatabase(database) {
-  console.log(csvFilePath);
+  fs.ReadStream(csvFilePath)
+    .pipe(parse({ columns: true }))
+    .on("data", async (row) => {
+      await database.insert(TABELE_DATABASE, {
+        id: randomUUID(),
+        title: row.title,
+        description: row.description,
+        created_at: new Date(),
+        completed_at: null,
+        updated_at: null,
+      });
+    });
 }
